2c3239c429e42f7e77e522a1b1835bebd54223ea
roshankkumar <roshanachery@gmail.com>
2018-07-29 14:38:23 +0530

    push version 1 changes

--- src/main/java/com/frontend/config/SecurityConfig.java
+++ src/main/java/com/frontend/config/SecurityConfig.java
@@ -8,26 +8,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-/*import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.frontend.service.UserServiceImpl.UserSecurityService;*/
+//import com.frontend.service.UserServiceImpl.UserSecurityService;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
 
-    @Autowired
-    private UserSecurityService userSecurityService;
+    /*@Autowired
+    private UserSecurityService userSecurityService;*/
 
     private static final String SALT = "salt"; // Salt should be protected carefully
 
@@ -57,22 +56,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 antMatchers(PUBLIC_MATCHERS).
                 permitAll().anyRequest().authenticated();
 
-        http
+       // http.csrf().disable().cors().disable().formLogin().failureUrl("/index?error");
+       /* http
                 .csrf().disable().cors().disable()
                 .formLogin().failureUrl("/index?error").defaultSuccessUrl("/userFront").loginPage("/index").permitAll()
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/index?logout").deleteCookies("remember-me").permitAll()
                 .and()
-                .rememberMe();
+                .rememberMe();*/
     }
 
 
 
-    @Autowired
+   /* @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 //    	 auth.inMemoryAuthentication().withUser("user").password("password").roles("USER"); //This is in-memory authentication
         auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
-    }
+    }*/
 
 
 }
